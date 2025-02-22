8ec1689e36f87396e1523258be2f49dcffeb9413
Amankumar-033 <amanbkp9135@gmail.com>
2025-02-22 19:44:53 +0530

    "data saved thankfully"

--- script/amazon.js
+++ script/amazon.js
@@ -1,22 +1,22 @@
 products = [{
-    name:  'Black and Gray Athletic Cotton Socks - 6 Pairs',
     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name:  'Black and Gray Athletic Cotton Socks - 6 Pairs',
     rating:{
         stars:4.5,
         count:87,
     },
     priceCents:1090
 },{
-    name: 'Intermediate Size Basketball',
     image: 'images/products/intermediate-composite-basketball.jpg',
+    name: 'Intermediate Size Basketball',
     rating:{
         stars:4,
         count:127,
     },
     priceCents:2095
 },{
-    name: ' Adults Plain Cotton T-Shirt - 2 Pack',
     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name: ' Adults Plain Cotton T-Shirt - 2 Pack',
     rating:{
         stars:4.5,
         count:56
@@ -27,10 +27,11 @@ products = [{
 
 let accumulator = '';
 products.forEach((product) => {
-    accumulator += `<div class="product-container">
+    accumulator += 
+    `
+    <div class="product-container">
           <div class="product-image-container">
-            <img class="product-image"
-              src=${product.image}
+            <img class="product-image" src="${product.image}" />
           </div>
 
           <div class="product-name limit-text-to-2-lines">
@@ -40,13 +41,13 @@ products.forEach((product) => {
           <div class="product-rating-container">
             <img class="product-rating-stars"
               src="images/ratings/rating-${product.rating.stars*10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
-            </div>
+                <div class="product-rating-count link-primary">
+                ${product.rating.count}
+                </div>
           </div>
 
           <div class="product-price">
-            $${product.priceCents * 0.01}
+            $${(product.priceCents * 0.01).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -74,7 +75,10 @@ products.forEach((product) => {
           <button class="add-to-cart-button button-primary">
             Add to Cart
           </button>
-        </div>`
+    </div>
+    `;
 });
 
 console.log(accumulator);
+
+document.querySelector(".js-products-grid").innerHTML = accumulator;
