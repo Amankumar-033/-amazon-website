ffedbb3bf6cb38589f39e76335a2e42c91f5d5ae
Amankumar-033 <amanbkp9135@gmail.com>
2025-03-02 01:19:28 +0530

    "delivery dayjs esm-module input-radio"

--- script/checkout.js
+++ script/checkout.js
@@ -1,5 +1,7 @@
 import {calculateCartQuantity, cart,cartQuantity,removeElementFromCart,} from '../data/cart.js';
 import {products} from '../data/products.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { deliveryOptions } from '../data/deliveryOption.js';
 
 
 window.onload = updateCheckoutQuantity;
@@ -54,48 +56,49 @@ cart.forEach((cartItem) => {
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-
-                <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input"
-                    name=${matching.id}>
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio" checked class="delivery-option-input"
-                    name=${matching.id}>
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input"
-                    name=${matching.id}>
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
+                ${createDeliveryOptions(matching,cartItem)}
               </div>
             </div>
           </div>
     `;
 });
+
+
+
+function createDeliveryOptions(matching,cartItem){
+    let html = '';
+
+    deliveryOptions.forEach((deliveryOption) => {
+    const day = dayjs();
+    const deliverydate = day.add(deliveryOption.deliveryDays,'days'); 
+    const deliveryString = deliverydate.format('dddd, MMMM D');
+    const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${deliveryOption.priceCents*0.01}`;
+    
+    console.log("aman " + cartItem.deliveryOptionId);
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+    console.log(deliveryOption.id);
+    console.log(cartItem.deliveryOptionId);
+
+
+    html += 
+      `
+      <div class="delivery-option">
+          <input type="radio" class="delivery-option-input"
+            ${isChecked ? 'checked' : ''} name=${matching.id}>
+          <div>
+            <div class="delivery-option-date">
+              ${deliveryString}
+            </div>
+            <div class="delivery-option-price">
+              ${priceString} Shipping
+            </div>
+          </div>
+      </div>
+      `;
+  });
+
+  return html;
+}
 
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
