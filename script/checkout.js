d1c3e101ccae03c2310c556e37bf6ea9ae70e317
Amankumar-033 <amanbkp9135@gmail.com>
2025-03-19 16:11:32 +0530

    promise alternate of callback

--- script/checkout.js
+++ script/checkout.js
@@ -2,9 +2,67 @@ import { loadProductsGrid } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
+
+
+
+
+new Promise((resolve)=>{
+    loadProductsGrid(()=>{
+        resolve();
+    });
+}).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+
+
+/*
+Nested promise one at a time..
+new Promise((resolve)=>{
+    loadProductsGrid(()=>{
+        resolve();
+    });
+}).then(()=>{
+   return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+   });
+}).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+
+/*
+Run simultaneously and then wait for all to complete
+Promise.all([
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    }),
+
+    new Promise((resolve)=>{
+        loadSomething(()=>{
+            resolve();
+        ]});
+    })
+
+]).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+*/
+
+
+/*
+Callback method avouid in large projects...
 loadProductsGrid(()=>{
     renderOrderSummary();
     renderPaymentSummary();     
 })
-
+*/
 
